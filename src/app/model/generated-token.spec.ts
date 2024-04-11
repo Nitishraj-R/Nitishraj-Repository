dbdf37d5d5299c7f1a7dc06a2db46bfcb8e95c68
Nitishraj-R <146335302+Nitishraj-R@users.noreply.github.com>
2024-04-11 20:40:54 +0530

    Angular Project

--- src/app/model/generated-token.spec.ts
+++
@@ -1,7 +0,0 @@
-import { GeneratedToken } from './generated-token';
-
-describe('GeneratedToken', () => {
-  it('should create an instance', () => {
-    expect(new GeneratedToken()).toBeTruthy();
-  });
-});
--- src/app/model/response-structure.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ResponseStructure } from './response-structure';
-
-describe('ResponseStructure', () => {
-  it('should create an instance', () => {
-    expect(new ResponseStructure()).toBeTruthy();
-  });
-});
